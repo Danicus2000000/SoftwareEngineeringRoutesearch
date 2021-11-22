3d9e4511ba84e907fee3422d85384dcc33acbd6b
DANIEL BULMAN <D.BULMAN-2020@hull.ac.uk>
2021-11-22 09:59:09 +0000

    Added Empty Class FIles for each method of path finding, ammended the test data and added the help command to the CMD

--- graphsearch/Program.cs
+++ graphsearch/Program.cs
@@ -6,7 +6,48 @@ namespace graphsearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 0) 
+            {
+                for(int i=0; i < args.Length; i++) 
+                {
+                    try 
+                    {
+                        switch (args[i]) 
+                        {
+                            case "-h":
+                                Console.WriteLine("-s <number or string>: Specify starting node, either as an integer or a string.  If a string, it must be in quotes, such as \"A\" or \"Norwich\".");
+                                Console.WriteLine("-e <number or string>: Specify end node either as an integer or a string.  If a string, it must be in quotes, such as \"Z\" or \"Zetland\".");
+                                Console.WriteLine("-a BF|DIJKSTRA|ASTAR: Use the relevant algorithm (brute force, Dijkstra's or A*)");
+                                Console.WriteLine("-f <filename>: read data from <filename> as the input");
+                                Console.WriteLine("-o <filename>: Write the output to file <filename> only");
+                                Console.WriteLine("-O <filename>: Write the output to both the console and file <filename>");
+                                Console.WriteLine("-d: Output debugging information to the console.");
+                                Console.WriteLine("-h: Shows the help dialog");
+                                break;
+                        }
+                    }
+                    catch (Exception) 
+                    {
+                        Console.WriteLine("An invalid Argument was given!");
+                    }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("No arguments were given!");
+            }
+        }
+        private void bruteForce() 
+        {
+
+        }
+        private void dijkstras() 
+        {
+
+        }
+        private void aStar() 
+        {
+
         }
     }
 }
