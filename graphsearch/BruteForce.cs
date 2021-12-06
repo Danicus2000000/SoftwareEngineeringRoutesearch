24089bb480adff88024ad6f83860cef782ce7e76
Danicus2000000 <dbulman68@gmail.com>
2021-12-06 16:47:11 +0000

    ASTAR works as intended as far as i can see Dijkstra still needs bug fix Brute Force seems to work well

--- graphsearch/BruteForce.cs
+++ graphsearch/BruteForce.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace graphsearch
 {
     public class BruteForce
@@ -14,27 +14,74 @@ namespace graphsearch
         /// <param name="endNode">A refrence to the end node in the list</param>
         /// <param name="adjacencyMatrix">A matrix describing the relation between nodes</param>
         /// <returns>A string describing the lowest cost path</returns>
-        public string Run(List<Node> nodes,Node startNode,Node endNode, int[,] adjacencyMatrix) 
+        public string Run(List<Node> nodes, Node startNode, Node endNode, int[,] adjacencyMatrix)
         {
-            GetInformation infoParse = new GetInformation();
-            Node currentNode= startNode;
-            int adjacencyIndex=nodes.IndexOf(currentNode);
-            while (currentNode != endNode)
+            GetInformation infoParse=new GetInformation();
+            Node currentNode = startNode;
+            List<List<Node>> paths = new List<List<Node>>() { new List<Node>() { currentNode } };
+            List<Node> open = new List<Node>() { currentNode };
+            List<Node> closed = new List<Node>();
+            while (open.Count != 0 && currentNode != endNode)//loop until either the open list is empty or the end node is found
             {
-                for (int j = 0; j < adjacencyMatrix.GetLength(1); j++)//loops through all nodes that could be accociated to the current node
+                int iToSearch = nodes.IndexOf(open[0]);
+                for (int j = 0; j < adjacencyMatrix.GetLength(1); j++)
                 {
-                    if (adjacencyMatrix[adjacencyIndex, j] != 0)//if there is a link
+                    if (adjacencyMatrix[iToSearch, j] != 0 && !open.Contains(nodes[j]) && !closed.Contains(nodes[j]))
                     {
-                        nodes[j].distanceFromStartNode = currentNode.distanceFromStartNode + adjacencyMatrix[adjacencyIndex, j];//set temp distance to be distance from start
-                        if (nodes[j].distanceFromStartNode < nodes[j].totalDistance)//if this is more efficient than previous route
+                        open.Add(nodes[j]);
+                        nodes[j].distanceFromStartNode = adjacencyMatrix[iToSearch, j] + currentNode.distanceFromStartNode;
+                        nodes[j].previousNode = nodes[iToSearch].name;
+                        for (int pathToClone = 0; pathToClone < paths.Count; pathToClone++)
                         {
-                            nodes[j].totalDistance=nodes[j].distanceFromStartNode;//change node to use this route
-                            nodes[j].previousNode = nodes[adjacencyIndex].name;
+                            if (paths[pathToClone][paths[pathToClone].Count - 1] == currentNode)
+                            {
+                                List<Node> clone = new List<Node>();
+                                for (int element = 0; element < paths[pathToClone].Count - 1; element++)
+                                {
+                                    clone.Add(paths[pathToClone][element]);
+                                }
+                                clone.Add(nodes[j]);
+                                paths.Add(clone);
+                                break;
+                            }
+                        }
+                        if (currentNode.isStartNode)
+                        {
+                            paths.Add(new List<Node>() { currentNode, nodes[j] });
+                        }
+                        else
+                        {
+                            bool found = false;//if a path is found add the new node on the end of it
+                            for (int pathInPaths = 0; pathInPaths < paths.Count - 1; pathInPaths++)
+                            {
+                                if (paths[pathInPaths][paths[pathInPaths].Count - 1] == currentNode)
+                                {
+                                    paths[pathInPaths].Add(nodes[j]);
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (!found)//if no path is found create a clone of the previous path FIX
+                            {
+                                List<Node> pathClone = new List<Node>();
+                                foreach (Node node in paths[paths.Count - 1])
+                                {
+                                    pathClone.Add(node);
+                                }
+                                paths.Add(pathClone);
+                                paths[paths.Count - 1].Remove(paths[paths.Count - 1][paths[paths.Count - 1].Count - 1]);
+                                paths[paths.Count - 1].Add(nodes[j]);
+                            }
                         }
                     }
+
                 }
-                //once all adjacent nodes for current have been checked we remove the cheapest node and make it the current node
-                currentNode = infoParse.getCheapestNode(nodes);
+                open.Remove(currentNode);
+                closed.Add(currentNode);
+                if (open.Count != 0)
+                {
+                    currentNode = open[0];
+                }
             }
             List<string> pathToAdd = infoParse.getTakenPath(nodes, startNode, endNode, adjacencyMatrix, out int totalCost);
             return infoParse.BuildPathFromStartToEnd(pathToAdd, totalCost);
