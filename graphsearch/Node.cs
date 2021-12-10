97a0c8775feb2f23bd392336d9eea6e3d738c02b
Danicus2000000 <dbulman68@gmail.com>
2021-12-10 22:25:25 +0000

    Removed unessasary value from node class

--- graphsearch/Node.cs
+++ graphsearch/Node.cs
@@ -10,7 +10,6 @@ namespace graphsearch
         public int nodeIndex { get; private set; }//node index
         public double xCoord { get; private set; }//the x coordinate of this node
         public double yCoord { get; private set; }//the y coordinate of this node
-        public double distanceFromStartNode = 0;//distance from start node at any given time during runtime
         public double heuristicDistance = 0;//distance from end node ignoring all obsticals
         public string previousNode=null;//previous node used to traverse to this point
         public double totalDistance = double.PositiveInfinity;//calculates total distance including from start and heuristic
